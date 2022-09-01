3735637e0bda75ce113c4228387c81c94446ff66
kfleung129 <endyjjhk@gmail.com>
2022-09-01 13:49:38 +0800

    added category badge

--- components/postItem.js
+++ components/postItem.js
@@ -1,11 +1,14 @@
 import style from '../styles/Home.module.css';
-import { Container } from '@chakra-ui/react';
+import { Container, HStack, Tag } from '@chakra-ui/react';
 function PostItem({ post }) {
     return (
         <Container w='2xl'>
-            <h2>
-                {post.id} {post.title}
-            </h2>
+            <HStack h='60px'>   
+                <h2>
+                    {post.id} {post.title}
+                </h2>
+                <Tag>{post.category}</Tag>
+            </HStack>
             <p>{post.description}</p>
         </Container>
     );
